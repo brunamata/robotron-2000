20cecab70c5dbdb20e14675f9a50ebcc53c9b7d9
Bruna <brunamata@usp.br>
2023-02-10 10:33:41 -0300

    poder eh dinamicamente atualizado pelas pecas

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const pecas = {
 
     "blindagem": {
         "forca": 41,
-        "poder": 20,
+        "poder": -20,
         "energia": 0,
-        "velocidade": -20
+        "velocidade": 20
     },
     "nucleos":{
         "forca": 0,
@@ -33,10 +33,26 @@ const pecas = {
     }
 }
 
-const poderesList = document.querySelectorAll('[poder]'); //nome e valor de cada poder
+const poderesList = document.querySelectorAll('[data-poder]'); //nome de cada poder
 const pecasList = document.querySelectorAll('[data-peca]'); //nome das pecas do robo
 const operacaoOption = document.querySelectorAll('[data-operacao]'); //todos os botoes com - e +
+const iniciarProdButton = document.querySelector('#producao');
 
+console.log(poderesList);
+
+
+
+iniciarProdButton.addEventListener("click", (event) =>{
+    poderesList.forEach( (element) =>{
+        element.textContent = parseInt(element.textContent) - parseInt(element.textContent);
+    })
+
+    operacaoOption.forEach( (element) => {
+        const divPeca = element.parentNode;
+        const peca = divPeca.querySelector('[valorPeca]');
+        peca.value = parseInt(peca.value) - parseInt(peca.Value);
+    })
+})
 
 //aqui analisa cada botao (element) das operacoes de - e +
 //caso aconteca um click em algum deles, ocorre a funcao que passa o evento como parametro e nela chama a funcao que
@@ -44,7 +60,8 @@ const operacaoOption = document.querySelectorAll('[data-operacao]'); //todos os 
 //event.target => pega o elem html que causou o evento .[tipo] .[nomeEspecifico] => pega a info específica
 operacaoOption.forEach((element) => {
     element.addEventListener("click", (event) => {
-        manipulaValorPeca(event.target.dataset.operacao, event.target.parentNode);
+        manipulaValorPeca(event.target.dataset.operacao, event.target.parentNode); //valor do data-operacao e da div pai
+        atualizaPoderes(event.target.dataset.peca, event.target.dataset.operacao, event.target.parentNode);  //valor do data-peca
     })
 
 })
@@ -53,7 +70,7 @@ operacaoOption.forEach((element) => {
 //OPERACAO: eh assim que sabe se tem que somar ou subtrair o valor
 //DIV PECA: passa a div anterior pra usar o querySelector e selecionar a primeira ocorrencia do data-attribute valorPeca
 function manipulaValorPeca(operacao, divPeca){
-    valorPeca = divPeca.querySelector('[valorPeca]');
+    const valorPeca = divPeca.querySelector('[valorPeca]');
 
     if(operacao === "subtrair"){
         if(parseInt(valorPeca.value) > 0){
@@ -62,4 +79,18 @@ function manipulaValorPeca(operacao, divPeca){
     }else{
         valorPeca.value = parseInt(valorPeca.value) + 1; 
     } 
+}
+
+function atualizaPoderes(peca , operacao, divPeca){
+    const poderValues = pecas[peca];  //eh o objeto que contem os valores de poderes da causados pela peca especifica
+    const pecaValue = divPeca.querySelector("[valorPeca]");
+
+    poderesList.forEach( (poderzinho) => {
+            if(operacao !== "subtrair"){
+                poderzinho.textContent = parseInt(poderzinho.textContent) + poderValues[poderzinho.dataset.poder];
+            } else if(parseInt(pecaValue.value) > 0){
+                poderzinho.textContent = parseInt(poderzinho.textContent) - poderValues[poderzinho.dataset.poder];
+            }
+        })
+
 }
